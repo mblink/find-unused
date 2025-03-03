b2f60c669145561ea31e8dab9770247c831fc8cc
Matt Dziuban <mrdziuban@gmail.com>
2025-03-03 17:11:19 -0500

    Flesh out unused givens support.

--- build.sbt
+++ build.sbt
@@ -1,14 +1,25 @@
 Global / onChangedBuildSource := ReloadOnSourceChanges
 
+// To develop tasty-query locally, run sbt with the environment variable `TASTY_QUERY_DEVELOPMENT=1`
+lazy val tastyQuery = ProjectRef(file(sys.env("HOME")) / "tasty-query", "tastyQueryJVM")
+lazy val tastyQueryDev = sys.env.get("TASTY_QUERY_DEVELOPMENT").exists(_ == "1")
+
 lazy val root = project.in(file("."))
   .settings(
     name := "tasty-query-unused",
     organization := "bondlink",
-    scalaVersion := "3.5.2",
+    scalaVersion := "3.6.3",
     version := "0.1.0-SNAPSHOT",
-    libraryDependencies ++= Seq(
-      "ch.epfl.scala" %% "tasty-query" % "1.4.0",
+    libraryDependencies ++= (
+      if (tastyQueryDev) Seq()
+      else Seq("ch.epfl.scala" %% "tasty-query" % "1.5.0")
+    ) ++ Seq(
       "com.lihaoyi" %% "os-lib" % "0.11.3",
-      "org.typelevel" %% "cats-core" % "2.12.0",
-    )
+      "com.lihaoyi" %% "pprint" % "0.9.0",
+      "org.typelevel" %% "cats-core" % "2.13.0",
+    ),
+    run / fork := true,
+    run / javaOptions += "-Xmx8G"
   )
+  .dependsOn((if (tastyQueryDev) Seq[ClasspathDep[ProjectReference]](tastyQuery) else Seq())*)
+  .aggregate((if (tastyQueryDev) Seq(tastyQuery) else Seq())*)
--- src/main/scala/bondlink/FindUnused.scala
+++ src/main/scala/bondlink/FindUnused.scala
@@ -1,6 +1,7 @@
 package bondlink
 
-import cats.Monoid
+import cats.{Id, Monoid}
+import cats.data.{Kleisli, Reader}
 import cats.syntax.all.*
 import java.net.URI
 import java.nio.file.FileSystems
@@ -10,45 +11,24 @@ import tastyquery.Trees.*
 import tastyquery.Types.*
 import tastyquery.jdk.ClasspathLoaders
 
-object Example {
-  trait Thing[A]
-
-  object Thing {
-    given thingO[A: Thing]: Thing[Option[A]] = new Thing[Option[A]] {}
-  }
-
-  case class Foo()
-  object Foo {
-    given thing: Thing[Foo] = new Thing[Foo] {}
-
-    // given show: cats.Show[Foo] = cats.Show.fromToString
-
-  }
-
-  // def takesShow[A](a: A)(using s: cats.Show[A]): String = s.show(a)
-
-  def takesThing[A](using t: Thing[A]): Thing[A] = t
-
-  // val test = takesShow(Foo())
-
-  val testThing = takesThing[Option[Foo]]
-}
-
 object FindUnusedGivens {
-  val jars = List(
-    os.Path("/Users/matt/tasty-query-unused/target/scala-3.5.2/tasty-query-unused_3-0.1.0-SNAPSHOT.jar"),
-    os.Path("/Users/matt/Library/Caches/Coursier/v1/https/repo1.maven.org/maven2/org/scala-lang/scala3-library_3/3.5.2/scala3-library_3-3.5.2.jar"),
-    os.Path("/Users/matt/Library/Caches/Coursier/v1/https/repo1.maven.org/maven2/ch/epfl/scala/tasty-query_3/1.4.0/tasty-query_3-1.4.0.jar"),
-    os.Path("/Users/matt/Library/Caches/Coursier/v1/https/repo1.maven.org/maven2/com/lihaoyi/os-lib_3/0.11.3/os-lib_3-0.11.3.jar"),
-    os.Path("/Users/matt/Library/Caches/Coursier/v1/https/repo1.maven.org/maven2/org/typelevel/cats-core_3/2.12.0/cats-core_3-2.12.0.jar"),
-    os.Path("/Users/matt/Library/Caches/Coursier/v1/https/repo1.maven.org/maven2/org/scala-lang/scala-library/2.13.14/scala-library-2.13.14.jar"),
-    os.Path("/Users/matt/Library/Caches/Coursier/v1/https/repo1.maven.org/maven2/com/lihaoyi/geny_3/1.1.1/geny_3-1.1.1.jar"),
-    os.Path("/Users/matt/Library/Caches/Coursier/v1/https/repo1.maven.org/maven2/org/typelevel/cats-kernel_3/2.12.0/cats-kernel_3-2.12.0.jar"),
-  ) // ++ os.walk(os.root / "example" / "proj" / "target" / "docker" / "stage").filter(_.ext == "jar")
+  val jars = os.walk(os.root / "example" / "proj" / "target" / "docker" / "stage").filter(_.ext == "jar")
 
   val paths = FileSystems.getFileSystem(URI.create("jrt:/")).getPath("modules", "java.base") :: jars.map(_.toNIO).toList
   val cp = ClasspathLoaders.read(paths)
   given ctx: Context = Context.initialize(cp)
+
+  private val pkgs = List("bl", "ixk", "common", "admin")
+
+  def allGivens: Givens = pkgs.foldMap(p => symGivens(ctx.findPackage(p))).run(Env(false, Set()))
+
+  def main(args: Array[String]): Unit = {
+    val g = allGivens
+    val unused = g.defined.filterNot(t => g.used.contains(t._1))
+    println(unused.toList.sortBy(_._2).mkString("\n"))
+  }
+
+  case class Env(log: Boolean, seenSymbols: Set[Int])
 
   case class Givens(
     defined: Map[Int, String],
@@ -61,108 +41,145 @@ object FindUnusedGivens {
 
     lazy val empty: Givens = monoid.empty
 
-    def defined(sym: Symbol): Givens = Givens(Map(sym.hashCode -> sym.displayFullName), Set.empty)
-    def used(sym: Symbol): Givens = Givens(Map.empty, Set(sym.hashCode))
+    private def symName(sym: Symbol): String =
+      Option(sym.owner) match {
+        case Some(owner: PackageSymbol) => s"${owner.displayFullName}.${sym.name}"
+        case Some(owner: TermOrTypeSymbol) => s"${symName(owner)}.${sym.name}"
+        case None => sym.displayFullName
+      }
 
-    def fromTermSymbol(sym: TermSymbol, mk: Symbol => Givens): Givens =
-      if (sym.isGivenOrUsing || sym.isImplicit) mk(sym) else Givens.empty
+    def defined(sym: TermSymbol): Givens =
+      Givens(
+        defined = Map(sym.hashCode -> symName(sym)),
+        // If a symbol overrides another, consider it used
+        // This accounts for cases where a `given` is used in a parent class, but not in the subclass
+        used = sym.nextOverriddenSymbol.fold(Set.empty)(_ => Set(sym.hashCode)),
+      )
+
+    def used(sym: Symbol): Givens =
+      Givens(Map.empty, Set(sym.hashCode))
+
+    def fromTermSymbol(sym: TermSymbol, mk: TermSymbol => Givens): Givens =
+      if (sym.isGivenOrUsing || sym.isImplicit) mk(sym) else empty
+
+    def fromTermOrPackageSymbol(sym: => TermSymbol | PackageSymbol, mk: TermSymbol => Givens): Givens =
+      Either.catchNonFatal(sym match {
+        case t: TermSymbol => fromTermSymbol(t, mk)
+        case _: PackageSymbol => empty
+      }).getOrElse(empty)
   }
 
-  private def treeGivensO(tree: Option[Tree]): Givens = tree.fold(Givens.empty)(treeGivens)
+  type ResF[A] = Reader[Env, A]
+  type Res = ResF[Givens]
 
-  private def treeGivensL(trees: List[Tree]): Givens = trees.foldMap(treeGivens)
+  private lazy val empty: Res = Givens.empty.pure[ResF]
 
-  def treeGivens(tree: Tree): Givens =
-    tree match {
-      case Alternative(trees) => treeGivensL(trees)
-      case AnnotatedTypeTree(tpt, annotation) => treeGivens(tpt) |+| treeGivens(annotation)
-      case AppliedTypeTree(tycon, args) => treeGivens(tycon) |+| treeGivensL(args)
-      case Apply(fun, args) => treeGivens(fun) |+| treeGivensL(args)
-      case Assign(lhs, rhs) => treeGivens(lhs) |+| treeGivens(rhs)
-      case Bind(_, body, symbol) => treeGivens(body)/* |+| symGivens(symbol)*/ // TODO - is symGivens right?
-      case Block(stats, expr) => treeGivensL(stats) |+| treeGivens(expr)
-      case ByNameTypeTree(result) => treeGivens(result)
-      case CaseDef(pattern, guard, body) => treeGivens(pattern) |+| treeGivensO(guard) |+| treeGivens(body)
-      case ClassDef(_, rhs, symbol) => treeGivens(rhs) |+| symGivens(symbol)
-      case DefDef(_, paramLists, resultTpt, rhs, symbol) =>
-        paramLists.foldMap(e => treeGivensL(e.merge)) |+|
-          treeGivens(resultTpt) |+|
-          treeGivensO(rhs)/* |+|
-          symGivens(symbol)*/ // TODO - is symGivens right?
-      case ExplicitTypeBoundsTree(low, high) => treeGivens(low) |+| treeGivens(high)
-      case Export(expr, selectors) => treeGivens(expr) |+| treeGivensL(selectors)
-      case ExprPattern(expr) => treeGivens(expr)
-      case i @ Ident(_) =>
-        Either.catchNonFatal(i.symbol match {
-          case t: TermSymbol => Givens.fromTermSymbol(t, Givens.used)
-          case _: PackageSymbol => Givens.empty
-        }).getOrElse(Givens.empty)
-      case If(cond, thenPart, elsePart) => treeGivens(cond) |+| treeGivens(thenPart) |+| treeGivens(elsePart)
-      case Import(expr, selectors) => treeGivens(expr) |+| treeGivensL(selectors)
-      case ImportIdent(_) => Givens.empty
-      case ImportSelector(imported, renamed, bound) => treeGivens(imported) |+| treeGivensO(renamed) |+| treeGivensO(bound)
-      case InferredTypeBoundsTree(_) => Givens.empty
-      case Inlined(expr, caller, bindings) => treeGivens(expr) |+| treeGivensO(caller) |+| treeGivensL(bindings)
-      case InlinedTypeTree(caller, expansion) => treeGivensO(caller) |+| treeGivens(expansion)
-      case InlineIf(cond, thenPart, elsePart) => treeGivens(cond) |+| treeGivens(thenPart) |+| treeGivens(elsePart)
-      case InlineMatch(selector, cases) => treeGivensO(selector) |+| treeGivensL(cases)
-      case Lambda(meth, tpt) => treeGivens(meth) |+| treeGivensO(tpt)
-      case Literal(_) => Givens.empty
-      case Match(selector, cases) => treeGivens(selector) |+| treeGivensL(cases)
-      case MatchTypeTree(bound, selector, cases) => treeGivens(bound) |+| treeGivens(selector) |+| treeGivensL(cases)
-      case NamedArg(_, arg) => treeGivens(arg)
-      case NamedTypeBoundsTree(_, _) => Givens.empty
-      case New(tpt) => treeGivens(tpt)
-      case OpaqueTypeAliasDefinitionTree(bounds, alias) => treeGivens(bounds) |+| treeGivens(alias)
-      case PackageDef(pid, stats) => symGivens(pid) |+| treeGivensL(stats) // TODO - is symGivens right? probz not
-      case PolyTypeDefinitionTree(tparams, body) => treeGivensL(tparams) |+| treeGivens(body)
-      case Quote(body, _) => treeGivens(body)
-      case QuotePattern(bindings, body, quotes, _) => treeGivensL(bindings) |+| treeGivens(body.merge) |+| treeGivens(quotes)
-      case RefinedTypeTree(underlying, refinements, refinedCls) =>
-        treeGivens(underlying) |+| treeGivensL(refinements) |+| symGivens(refinedCls) // TODO - is symGivens right?
-      case Return(expr, from) => treeGivensO(expr) |+| symGivens(from) // TODO - is symGivens right?
-      case Select(qualifier, _) => treeGivens(qualifier) // TODO - will this ever be a given?
-      case SelectOuter(qualifier, _) => treeGivens(qualifier) // TODO - will this ever be a given?
-      case SelectTypeTree(qualifier, _) => treeGivens(qualifier)
-      case SelfDef(_, tpt) => treeGivens(tpt)
-      case SeqLiteral(elems, elemstpt) => treeGivensL(elems) |+| treeGivens(elemstpt)
-      case SingletonTypeTree(ref) => treeGivens(ref)
-      case Splice(expr, _) => treeGivens(expr)
-      case SplicePattern(pattern, targs, args, _) => treeGivens(pattern) |+| treeGivensL(targs) |+| treeGivensL(args)
-      case Super(qual, mix) => treeGivens(qual) |+| treeGivensO(mix)
-      case Template(constr, parents, self, body) => treeGivens(constr) |+| treeGivensL(parents) |+| treeGivensO(self) |+| treeGivensL(body)
-      case TermRefTypeTree(qualifier, _) => treeGivens(qualifier) // TODO - will this ever be a given?
-      case This(qualifier) => treeGivens(qualifier)
-      case Throw(expr) => treeGivens(expr)
-      case Try(expr, cases, finalizer) => treeGivens(expr) |+| treeGivensL(cases) |+| treeGivensO(finalizer)
-      case TypeAliasDefinitionTree(alias) => treeGivens(alias)
-      case TypeApply(fun, args) => treeGivens(fun) |+| treeGivensL(args)
-      case TypeBindingsTree(bindings, body) => treeGivensL(bindings) |+| treeGivens(body)
-      case TypeCaseDef(pattern, body) => treeGivens(pattern) |+| treeGivens(body)
-      case Typed(expr, tpt) => treeGivens(expr) |+| treeGivens(tpt)
-      case TypeIdent(_) => Givens.empty
-      case TypeLambdaTree(tparams, body) => treeGivensL(tparams) |+| treeGivens(body)
-      case TypeMember(_, rhs, _) => treeGivens(rhs)
-      case TypeParam(_, bounds, _) => treeGivens(bounds)
-      case TypeTest(body, tpt) => treeGivens(body) |+| treeGivens(tpt)
-      case TypeTreeBind(_, body, _) => treeGivens(body)
-      case TypeWrapper(_) => Givens.empty
-      // TODO - should implicits be added to Givens#used?
-      case Unapply(fun, implicits, patterns) => treeGivens(fun) |+| treeGivensL(implicits) |+| treeGivensL(patterns)
-      case ValDef(_, tpt, rhs, symbol) =>
-        treeGivens(tpt) |+|
-          treeGivensO(rhs)/* |+|
-          symGivens(symbol)*/ // TODO - is symGivens right?
-      case While(cond, body) => treeGivens(cond) |+| treeGivens(body)
-      case WildcardPattern(_) => Givens.empty
-      case WildcardTypeArgTree(bounds) => treeGivens(bounds)
+  private def treeGivensO(tree: Option[Tree]): Res = tree.fold(empty)(treeGivens)
+
+  private def treeGivensL(trees: List[Tree]): Res = trees.foldMap(treeGivens)
+
+  def treeGivens(tree: Tree): Res =
+    Kleisli.ask[Id, Env].flatMap { env =>
+      if (env.log) println(s"************* tree: ${pprint(tree.toString)}")
+      tree match {
+        case Alternative(trees) => treeGivensL(trees)
+        case AnnotatedTypeTree(tpt, annotation) => treeGivens(tpt) |+| treeGivens(annotation)
+        case AppliedTypeTree(tycon, args) => treeGivens(tycon) |+| treeGivensL(args)
+        case Apply(fun, args) => treeGivens(fun) |+| treeGivensL(args)
+        case Assign(lhs, rhs) => treeGivens(lhs) |+| treeGivens(rhs)
+        case Bind(_, body, _ /* symbol */) => treeGivens(body)/* |+| symGivens(symbol)*/ // TODO - is symGivens right?
+        case Block(stats, expr) => treeGivensL(stats) |+| treeGivens(expr)
+        case ByNameTypeTree(result) => treeGivens(result)
+        case CaseDef(pattern, guard, body) => treeGivens(pattern) |+| treeGivensO(guard) |+| treeGivens(body)
+        case ClassDef(_, rhs, symbol) => treeGivens(rhs) |+| symGivens(symbol)
+        case DefDef(_, paramLists, resultTpt, rhs, _ /* symbol */) =>
+          paramLists.foldMap(e => treeGivensL(e.merge)) |+|
+            treeGivens(resultTpt) |+|
+            treeGivensO(rhs)/* |+|
+            symGivens(symbol)*/ // TODO - is symGivens right?
+        case ExplicitTypeBoundsTree(low, high) => treeGivens(low) |+| treeGivens(high)
+        case Export(expr, selectors) => treeGivens(expr) |+| treeGivensL(selectors)
+        case ExprPattern(expr) => treeGivens(expr)
+        case i @ Ident(_) => Givens.fromTermOrPackageSymbol(i.symbol, Givens.used).pure[ResF]
+        case If(cond, thenPart, elsePart) => treeGivens(cond) |+| treeGivens(thenPart) |+| treeGivens(elsePart)
+        case Import(expr, selectors) => treeGivens(expr) |+| treeGivensL(selectors)
+        case ImportIdent(_) => empty
+        case ImportSelector(imported, renamed, bound) => treeGivens(imported) |+| treeGivensO(renamed) |+| treeGivensO(bound)
+        case InferredTypeBoundsTree(_) => empty
+        case Inlined(expr, caller, bindings) => treeGivens(expr) |+| treeGivensO(caller) |+| treeGivensL(bindings)
+        case InlinedTypeTree(caller, expansion) => treeGivensO(caller) |+| treeGivens(expansion)
+        case InlineIf(cond, thenPart, elsePart) => treeGivens(cond) |+| treeGivens(thenPart) |+| treeGivens(elsePart)
+        case InlineMatch(selector, cases) => treeGivensO(selector) |+| treeGivensL(cases)
+        case Lambda(meth, tpt) => treeGivens(meth) |+| treeGivensO(tpt)
+        case Literal(_) => empty
+        case Match(selector, cases) => treeGivens(selector) |+| treeGivensL(cases)
+        case MatchTypeTree(bound, selector, cases) => treeGivens(bound) |+| treeGivens(selector) |+| treeGivensL(cases)
+        case NamedArg(_, arg) => treeGivens(arg)
+        case NamedTypeBoundsTree(_, _) => empty
+        case New(tpt) => treeGivens(tpt)
+        case OpaqueTypeAliasDefinitionTree(bounds, alias) => treeGivens(bounds) |+| treeGivens(alias)
+        case PackageDef(pid, stats) => symGivens(pid) |+| treeGivensL(stats)
+        case PolyTypeDefinitionTree(tparams, body) => treeGivensL(tparams) |+| treeGivens(body)
+        case Quote(body, _) => treeGivens(body)
+        case QuotePattern(bindings, body, quotes, _) => treeGivensL(bindings) |+| treeGivens(body.merge) |+| treeGivens(quotes)
+        case RefinedTypeTree(underlying, refinements, refinedCls) =>
+          treeGivens(underlying) |+| treeGivensL(refinements) |+| symGivens(refinedCls)
+        case Return(expr, from) => treeGivensO(expr) |+| symGivens(from)
+        case s @ Select(qualifier, _) => Givens.fromTermOrPackageSymbol(s.symbol, Givens.used).pure[ResF] |+| treeGivens(qualifier)
+        case SelectOuter(qualifier, _) => treeGivens(qualifier) // TODO - will this ever be a given?
+        case SelectTypeTree(qualifier, _) => treeGivens(qualifier)
+        case SelfDef(_, tpt) => treeGivens(tpt)
+        case SeqLiteral(elems, elemstpt) => treeGivensL(elems) |+| treeGivens(elemstpt)
+        case SingletonTypeTree(ref) => treeGivens(ref)
+        case Splice(expr, _) => treeGivens(expr)
+        case SplicePattern(pattern, targs, args, _) => treeGivens(pattern) |+| treeGivensL(targs) |+| treeGivensL(args)
+        case Super(qual, mix) => treeGivens(qual) |+| treeGivensO(mix)
+        case Template(constr, parents, self, body) => treeGivens(constr) |+| treeGivensL(parents) |+| treeGivensO(self) |+| treeGivensL(body)
+        case TermRefTypeTree(qualifier, _) => treeGivens(qualifier)
+        case This(qualifier) => treeGivens(qualifier)
+        case Throw(expr) => treeGivens(expr)
+        case Try(expr, cases, finalizer) => treeGivens(expr) |+| treeGivensL(cases) |+| treeGivensO(finalizer)
+        case TypeAliasDefinitionTree(alias) => treeGivens(alias)
+        case TypeApply(fun, args) => treeGivens(fun) |+| treeGivensL(args)
+        case TypeBindingsTree(bindings, body) => treeGivensL(bindings) |+| treeGivens(body)
+        case TypeCaseDef(pattern, body) => treeGivens(pattern) |+| treeGivens(body)
+        case Typed(expr, tpt) => treeGivens(expr) |+| treeGivens(tpt)
+        case TypeIdent(_) => empty
+        case TypeLambdaTree(tparams, body) => treeGivensL(tparams) |+| treeGivens(body)
+        case TypeMember(_, rhs, _) => treeGivens(rhs)
+        case TypeParam(_, bounds, _) => treeGivens(bounds)
+        case TypeTest(body, tpt) => treeGivens(body) |+| treeGivens(tpt)
+        case TypeTreeBind(_, body, _) => treeGivens(body)
+        case TypeWrapper(_) => empty
+        // TODO - should implicits be added to Givens#used?
+        case Unapply(fun, implicits, patterns) => treeGivens(fun) |+| treeGivensL(implicits) |+| treeGivensL(patterns)
+        case ValDef(_, tpt, rhs, _ /* symbol */) =>
+          treeGivens(tpt) |+|
+            treeGivensO(rhs)/* |+|
+            symGivens(symbol)*/ // TODO - is symGivens right?
+        case While(cond, body) => treeGivens(cond) |+| treeGivens(body)
+        case WildcardPattern(_) => empty
+        case WildcardTypeArgTree(bounds) => treeGivens(bounds)
+      }
     }
 
-  def symGivens(sym: Symbol): Givens =
-    sym match {
-      case t: TermSymbol => Givens.fromTermSymbol(t, Givens.defined) |+| t.tree.fold(Givens.empty)(treeGivens)
-      case p: PackageSymbol => p.declarations.foldMap(symGivens)
-      case c: ClassSymbol => c.declarations.foldMap(symGivens)
-      case _: (ClassTypeParamSymbol | LocalTypeParamSymbol | TypeMemberSymbol) => Givens.empty
+  def symGivens(sym: Symbol): Res =
+    Reader { env =>
+      if (env.seenSymbols.contains(sym.hashCode)) empty.run(env)
+      else {
+        val updEnv = env.copy(seenSymbols = env.seenSymbols + sym.hashCode)
+        sym match {
+          case t: TermSymbol =>
+            if (updEnv.log) println(s"*********** TermSymbol ${t.name}")
+            (Givens.fromTermSymbol(t, Givens.defined).pure[ResF] |+| t.tree.fold(empty)(treeGivens)).run(updEnv)
+          case p: PackageSymbol =>
+            if (updEnv.log) println(s"*********** PackageSymbol ${p.name}")
+            p.declarations.foldMap(symGivens).run(updEnv)
+          case c: ClassSymbol =>
+            if (updEnv.log) println(s"*********** ClassSymbol ${c.name}")
+            (c.tree.fold(empty)(treeGivens) |+| c.declarations.foldMap(symGivens)).run(updEnv)
+          case _: (ClassTypeParamSymbol | LocalTypeParamSymbol | TypeMemberSymbol) => empty.run(updEnv)
+        }
+      }
     }
 }
