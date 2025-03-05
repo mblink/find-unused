4b4d16786b9235634793bbdd284a25dda5583ba9
Matt Dziuban <mrdziuban@gmail.com>
2025-03-05 15:33:27 -0500

    Revert native image plugin changes.

--- plugin/src/main/scala/bondlink/FindUnusedPlugin.scala
+++ plugin/src/main/scala/bondlink/FindUnusedPlugin.scala
@@ -3,34 +3,22 @@ package bondlink
 import coursier.Fetch
 import coursier.core.*
 import coursier.maven.MavenRepository
-import dev.dirs.BaseDirectories
-import java.io.{FileOutputStream, PrintWriter, StringWriter}
-import java.net.URI
-import java.nio.channels.Channels
+import java.io.{PrintWriter, StringWriter}
 import sbt.*
 import sbt.internal.util.complete.*
 import sbt.internal.util.complete.Parsers.*
 import sbt.Keys.*
 import sbt.plugins.JvmPlugin
-import scala.sys.process.*
 
 object FindUnusedPlugin extends AutoPlugin {
-  sealed trait CliRunnable
-  case class CliBinary(path: File) extends CliRunnable
-  case class CliClasspath(cp: Seq[File]) extends CliRunnable
-
   object autoImport {
     val findUnused = settingKey[Unit]("Find unused")
 
-    val findUnusedCliBinaryDownload = taskKey[CliRunnable]("Find unused CLI binary download")
+    val findUnusedCliDownload = taskKey[Seq[File]]("Find unused CLI download")
 
-    val findUnusedCliCoursierDownload = taskKey[CliClasspath]("Find unused CLI coursier download")
+    val findUnusedCliClasspath = taskKey[Seq[File]]("Find unused CLI classpath")
 
-    val findUnusedCliRunnable = taskKey[CliRunnable]("Find unused CLI runnable")
-
-    val findUnusedCommandOptions = taskKey[(CliRunnable, Seq[String])]("Find unused command options")
-
-    val findUnusedUseCoursier = settingKey[Boolean]("Find unused use coursier")
+    val findUnusedCommandOptions = taskKey[(ForkOptions, Seq[String], Seq[String])]("Find unused command options")
 
     val findUnusedUseLocalClasspath = settingKey[Boolean]("Find unused use local classpath")
 
@@ -59,11 +47,9 @@ object FindUnusedPlugin extends AutoPlugin {
 
   override lazy val globalSettings: Seq[Setting[?]] = Def.settings(
     findUnused / javaOptions := Seq.empty,
-    findUnusedCliBinaryDownload := findUnusedCliBinaryDownloadTask.value,
-    findUnusedCliCoursierDownload := findUnusedCliCoursierDownloadTask.value,
-    findUnusedCliRunnable := findUnusedCliRunnableTask.value,
+    findUnusedCliDownload := findUnusedCliDownloadTask.value,
+    findUnusedCliClasspath := findUnusedCliClasspathTask.value,
     findUnusedCommandOptions := findUnusedCommandOptionsTask.value,
-    findUnusedUseCoursier := false,
     findUnusedUseLocalClasspath := false,
     findUnusedStoreProjectClasspaths := findUnusedStoreProjectClasspathsTask.evaluated,
     findUnusedPackages := Seq.empty,
@@ -72,7 +58,7 @@ object FindUnusedPlugin extends AutoPlugin {
   )
 
   // https://stackoverflow.com/a/39868021/2163024
-  private def autoClose0[A <: AutoCloseable, B](a: A)(f: A => B): B = {
+  private def autoClose[A <: AutoCloseable, B](a: A)(f: A => B): B = {
     var err: Throwable = null
     try { f(a) }
     catch {
@@ -97,8 +83,8 @@ object FindUnusedPlugin extends AutoPlugin {
   private def runLogged[A](log: Logger, f: () => A): A =
     scala.util.Try(f()).fold(
       e => {
-        autoClose0(new StringWriter())(sw =>
-          autoClose0(new PrintWriter(sw)) { pw =>
+        autoClose(new StringWriter())(sw =>
+          autoClose(new PrintWriter(sw)) { pw =>
             e.printStackTrace(pw)
             log.error(sw.toString)
           }
@@ -108,80 +94,19 @@ object FindUnusedPlugin extends AutoPlugin {
       identity,
     )
 
-  private def autoClose[A <: AutoCloseable, B](log: Logger, get: () => A)(f: A => B): B =
-    autoClose0(runLogged(log, get))(f)
+  private lazy val findUnusedCliDownloadTask = Def.task {
+    val log = streams.value.log
 
-  private def sysProp(name: String): Option[String] = Option(System.getProperty(name)).map(_.toLowerCase)
-
-  private def isLinux(os: String): Boolean = os.contains("linux")
-  private def isMac(os: String): Boolean = os.contains("mac")
-  private def isWindows(os: String): Boolean = os.contains("windows")
-
-  private def isARM(arch: String): Boolean = arch.contains("aarch64") || arch.contains("arm64")
-  private def isX64(arch: String): Boolean = arch.contains("x86_64") || arch.contains("amd64")
-
-  private def coursierDownload(log: Logger): CliClasspath = {
     log.info("Fetching find-unused CLI with coursier")
+
     val dep = Dependency(Module(Organization("bondlink"), ModuleName("find-unused-cli_3"), Map.empty), BuildInfo.version)
     val repos = Seq(MavenRepository("https://raw.githubusercontent.com/mblink/maven-repo/main"))
-    val cp = runLogged(log, () => Fetch().withRepositories(repos).addDependencies(dep).run())
-    CliClasspath(cp)
+    runLogged(log, () => Fetch().withRepositories(repos).addDependencies(dep).run())
   }
 
-  private lazy val findUnusedCliBinaryDownloadTask = Def.task {
-    val log = streams.value.log
-    val cliBinaryName = (sysProp("os.name"), sysProp("os.arch")) match {
-      case (Some(os), Some(arch)) if isLinux(os) && isARM(arch) => Some("find-unused-linux-arm")
-      case (Some(os), Some(arch)) if isLinux(os) && isX64(arch) => Some("find-unused-linux")
-      case (Some(os), Some(arch)) if isMac(os) && isARM(arch) => Some("find-unused-mac-arm")
-      case (Some(os), Some(arch)) if isMac(os) && isX64(arch) => Some("find-unused-mac")
-      case (Some(os), _) if isWindows(os) => Some("find-unused-windows.exe")
-      case _ => None
-    }
-
-    cliBinaryName match {
-      case Some(bin) =>
-        val binDir = file(BaseDirectories.get.cacheDir) / "find-unused" / BuildInfo.version
-        val binPath = binDir / bin
-        val binPathTgz = binDir / s"$bin.tar.gz"
-
-        log.info(s"Checking for binary $binPath")
-
-        if (!binPath.exists) {
-          val url = s"https://github.com/mblink/find-unused/releases/download/v${BuildInfo.version}/$bin.tar.gz"
-          log.info(s"Binary $binPath does not exist, downloading from $url...")
-
-          binDir.mkdirs
-
-          autoClose(log, () => Channels.newChannel(new URI(url).toURL.openStream))(channel =>
-            autoClose(log, () => new FileOutputStream(binPathTgz))(output =>
-              output.getChannel.transferFrom(channel, 0, Long.MaxValue)
-            )
-          )
-
-          log.info(s"Extracting $binPathTgz to $binPath")
-
-          Process(Seq("tar", "-xzf", s"$bin.tar.gz"), cwd = Some(binDir)).! match {
-            case 0 => ()
-            case code => throw new MessageOnlyException(s"Failed to extract binary (exit code $code)")
-          }
-        }
-
-        CliBinary(binPath)
-
-      case None =>
-        coursierDownload(log)
-    }
-  }
-
-  private lazy val findUnusedCliCoursierDownloadTask = Def.task {
-    coursierDownload(streams.value.log)
-  }
-
-  private lazy val findUnusedCliRunnableTask = Def.taskDyn {
-    if (findUnusedUseLocalClasspath.value) Def.task[CliRunnable](CliClasspath(BuildInfo.cliClasspath))
-    else if (findUnusedUseCoursier.value) Def.task[CliRunnable](findUnusedCliCoursierDownload.value)
-    else Def.task(findUnusedCliBinaryDownload.value)
+  private lazy val findUnusedCliClasspathTask = Def.taskDyn {
+    if (findUnusedUseLocalClasspath.value) Def.task[Seq[File]](BuildInfo.cliClasspath)
+    else Def.task(findUnusedCliDownload.value)
   }
 
   private val scalaVersionParser = {
@@ -231,7 +156,15 @@ object FindUnusedPlugin extends AutoPlugin {
   }
 
   private lazy val findUnusedCommandOptionsTask = Def.task {
-    val cliRunnable = findUnusedCliRunnable.value
+    val forkOpts = ForkOptions()
+      .withJavaHome(javaHome.value)
+      .withOutputStrategy(outputStrategy.value)
+      .withConnectInput(false)
+      .withRunJVMOptions((findUnused / javaOptions).value.toVector)
+
+    val cliClasspath = findUnusedCliClasspath.value
+    val mainClass = "bondlink.FindUnusedCli"
+    val scalaOpts = List("-classpath", Path.makeString(cliClasspath), mainClass)
 
     @annotation.tailrec
     def runCommand(command: String, state: State): State = {
@@ -256,27 +189,13 @@ object FindUnusedPlugin extends AutoPlugin {
 
     val cliOpts = projClasspath.flatMap(Seq("-c", _)) ++ projPackages.flatMap(Seq("-p", _))
 
-    (cliRunnable, cliOpts)
+    (forkOpts, scalaOpts, cliOpts)
   }
 
   private lazy val findUnusedGivensTask = Def.task {
-    val exitCode = findUnusedCommandOptions.value match {
-      case (CliBinary(cliBinary), cliOpts) =>
-        (cliBinary.toString +: "givens" +: cliOpts).!
+    val (forkOpts, scalaOpts, cliOpts) = findUnusedCommandOptions.value
 
-      case (CliClasspath(cliClasspath), cliOpts) =>
-        val forkOpts = ForkOptions()
-          .withJavaHome(javaHome.value)
-          .withOutputStrategy(outputStrategy.value)
-          .withConnectInput(false)
-          .withRunJVMOptions((findUnused / javaOptions).value.toVector)
-
-        val scalaOpts = List("-classpath", Path.makeString(cliClasspath), "bondlink.FindUnusedCli")
-
-        Fork.java.fork(forkOpts, scalaOpts ++ List("givens") ++ cliOpts).exitValue()
-    }
-
-    exitCode match {
+    Fork.java.fork(forkOpts, scalaOpts ++ List("givens") ++ cliOpts).exitValue() match {
       case 0 => ()
       case 1 => throw new MessageOnlyException("findUnusedGivens failed, see output above")
     }
