065146b12d845e7223f8267679fba21817d077f0
Matt Dziuban <mrdziuban@gmail.com>
2025-08-13 13:33:06 -0400

    Upgrade to sbt 2.0.0-RC2.

--- plugin/src/main/scala-3/bl/unused/FindUnusedPluginCompat.scala
+++ plugin/src/main/scala-3/bl/unused/FindUnusedPluginCompat.scala
@@ -3,7 +3,9 @@ package bl.unused
 import sbt.*
 import sbt.Keys.*
 
-private[unused] trait FindUnusedPluginCompat {
+private[unused] trait FindUnusedPluginCompat extends sbt.util.CacheImplicits {
+  export sbt.util.cacheLevel
+
   final lazy val findUnusedFullTestClasspathTask = Def.task {
     val converter = fileConverter.value
     (Test / fullClasspath).value.map(f => converter.toPath(f.data).toString)
